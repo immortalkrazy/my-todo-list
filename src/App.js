72b7805ffa1b5f0af7a01cd5132d17f811232422
Faisal Ibrahim <faisal.ibrahim@gmail.com>
2026-03-29 00:04:23 -0400

    more changes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import React, { useState } from 'react';
+import React, { useRef, useState, useEffect } from 'react';
 import TodoList from './TodoList';
+import { v4 as uuidv4 } from 'uuid';
+
+const LOCAL_STORAGE_KEY = 'todoApp.todos';
 
 function App() {
 
-  const [todos, setTodos] = useState([
-    { id: 1, name: 'Todo 1', completed: false },
-    { id: 2, name: 'Todo 2', completed: false },
-    { id: 3, name: 'Todo 3', completed: false }
-  ]);
+  const [todos, setTodos] = useState([]);
+  const todoNameRef = useRef();
+
+  useEffect(() => {
+    const storedTodos = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
+    if (storedTodos) {
+      setTodos(storedTodos);
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(todos));
+  }, [todos])
+
+  const handleAddTodo = (e) => {
+    e.preventDefault();
+
+    const name = todoNameRef.current.value;
+    if (name === '') return
+
+    setTodos(prevTodos => {
+      return [...prevTodos, { id: uuidv4(), name: name, completed: false }]
+    })
+
+    todoNameRef.current.value = null
+  }
 
   return (
     <>
       <TodoList todos={todos} />
-      <input type="text" />
-      <button>Add Todo</button>
+      <form onSubmit={handleAddTodo}>
+        <input ref={todoNameRef} type="text" />
+        <button type="submit">Add Todo</button>
+      </form>
       <button>Clean Completed</button>
       <div>0 left to do</div>
     </>
-
   )
 }
 
